9e0a82e4e33119b9a81139c519bd0da001edc528
Tony Osibov <tony@nuagesolutions.com>
2013-04-06 18:12:41 -0700

    added a label item renderer

---
+++ js/oj/list/LabelItemRenderer.js
@@ -0,0 +1,21 @@
+OJ.importJs('oj.components.ItemRenderer');
+OJ.importJs('oj.components.Label');
+
+
+OJ.extendClass(
+	OjItemRenderer, 'OjLabelItemRenderer',
+	{
+		'_template' : 'oj.list.LabelItemRenderer',
+
+
+		'_redrawData' : function(){
+			if(this._s('OjLabelItemRender', '_redrawData', arguments)){
+				this.lbl.setText(this._data);
+
+				return true;
+			}
+
+			return false;
+		}
+	}
+);
