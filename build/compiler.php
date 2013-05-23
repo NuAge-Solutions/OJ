caff041ae0aed5695e923cf3c64d21449b85bf92
Tony Osibov <tony.osibov@gmail.com>
2013-05-22 18:15:32 -0700

    added build dirs

--- build/compiler.php
+++ build/compiler.php
@@ -1,8 +1,6 @@
 <?php
 
-	# argument 1 = source path
-	# argument 2 = destination path
-	# arguments 3..n = includes and excludes
+	# arguments 1..n = excludes
 
 	// disable all error reporting except for important ones
 	error_reporting(E_ERROR);
@@ -236,6 +234,8 @@
 	}
 
 	function compress($dir, $error_msg = NULL){
+		global $destination;
+
 		if($handle = opendir($dir)){
 			while(false !== ($entry = readdir($handle))){
 				// ignore hidden files
@@ -249,11 +249,11 @@
 					compress($path, $error_msg ? $error_msg . DS . $entry : NULL);
 				}
 				else if(substr($entry, -4) == '.css' || substr($entry, -3) == '.js'){
-					exec('java -jar yuicompressor.jar -o ' . $path . ' ' . $path);
+					exec('java -jar ' . mkPath($destination, 'yuicompressor.jar') . ' -o ' . $path . ' ' . $path);
 				}
 				else if(substr($entry, -5) == '.html' || substr($entry, -4) == '.htm'){
 					exec(
-						'java -jar htmlcompressor.jar --remove-quotes --remove-intertag-spaces -o ' . $path . ' ' . $path
+						'java -jar ' . mkPath($destination, 'htmlcompressor.jar') . ' --remove-quotes --remove-intertag-spaces -o ' . $path . ' ' . $path
 					);
 				}
 			}
@@ -304,7 +304,9 @@
 	}
 
 	function empty_dir($path){
-		if(empty($path) || strpos($path, '/Volumes/Workspace/www/') !== 0){
+		global $destination;
+
+		if(empty($path) || $path == DS || strpos($path, $destination) !== 0){
 			return false;
 		}
 
@@ -357,160 +359,15 @@
 		flush();
 	}
 
-	function updateApps($source){
-		global $destination, $compiled_js, $compiled_css;
 
-		if(is_dir($source)){
-			if($handle = opendir($source)){
-				while(false !== ($entry = readdir($handle))){
-					// ignore hidden files
-					if($entry[0] == '.'){
-						continue;
-					}
-
-					$path = mkPath($source, $entry);
-
-					if(is_dir($path)){
-						updateApps($path);
-					}
-					else if(substr($entry, -5) == '.html'){
-						updateApps($path);
-					}
-				}
-
-				closedir($handle);
-			}
-		}
-		else{
-			$file = file_get_contents($source);
-
-			// remove js imports that have been compressed
-			$index = 0;
-
-			while(($index = strpos($file, 'OJ.importJs(', $index)) !== FALSE){
-				// offset for the func syntax
-				$start = $end = $index + 13;
-
-				// get the position of the end of the import statement
-				$end = strpos($file, ')', $index);
-
-				// raw include path
-				$include = mkPath($destination, 'js', str_replace('.', DS, substr($file, $start, ($end - 1) - $start)) . '.js');
-
-				// make sure we have already compile this js file before we remove it
-				if(in_array($include, $compiled_js)){
-					if(substr($file, $end + 1, 1) == ';'){
-						$end++;
-					}
-
-					$file = substr_replace(
-						$file,
-						'',
-						$index,
-						$end - $index
-					);
-				}
-				else{
-					// move the index forward so we don't end up in a loop
-					$index = $end;
-				}
-			}
-
-
-			// make js replacements
-			foreach($compiled_js as $path){
-				$path = str_replace(mkPath($destination, 'js', ''), '', $path);
-
-				$parts = explode(DS, $path);
-
-				$file = str_replace($path, $parts[0] . '.js', $file);
-			}
-
-
-			// remove css imports that have been compressed
-			$index = 0;
-
-			while(($index = strpos($file, 'OJ.importCss(', $index)) !== FALSE){
-				// offset for the func syntax
-				$start = $end = $index + 14;
-
-				// get the position of the end of the import statement
-				$end = strpos($file, ')', $index);
-
-				// raw include path
-				$include = mkPath($destination, 'css', str_replace('.', DS, substr($file, $start, ($end - 1) - $start)) . '.css');
-
-				// make sure we have already compile this js file before we remove it
-				if(in_array($include, $compiled_css)){
-					if(substr($file, $end + 1, 1) == ';'){
-						$end++;
-					}
-
-					$file = substr_replace($file, '', $index, $end - $index);
-				}
-				else{
-					// move the index forward so we don't end up in a loop
-					$index = $end;
-				}
-			}
-
-
-			// make css replacements
-			foreach($compiled_css as $path){
-				$path = str_replace(mkPath($destination, 'css', ''), '', $path);
-
-				$parts = explode(DS, $path);
-
-				$file = str_replace($path, $parts[0] . '.css', $file);
-			}
-
-			// save the changes
-			file_put_contents($source, $file);
-		}
-	}
-
-
-
-
-	// make sure we have a source path
-	if(($ln = count($argv)) < 2){
-		endScript('You must pass a source path.');
-	}
-
-	// make sure we have a destination path
-	if(($ln = count($argv)) < 3){
-		endScript('You must pass a destination path.');
-	}
-
-	// make sure we have at least one namespace
-	if($ln < 4){
-		endScript('You must pass at least one namespace to compress.');
-	}
-
-	// make sure the source exists
-	$source = realpath($argv[1]);
-
-	if(!file_exists($source)){
-		endScript('The source path does not exist.');
-	}
-
-	// make sure the source and destination are not the same
-	$destination = realpath($argv[2]);
-
-	if($source == $destination){
-		endScript('You source and destination may not be the same as it will overwrite the uncompiled code.');
-	}
-
-	// make sure the destination exists, otherwise create it
-	if(!file_exists($destination)){
-		trace('Attempting to auto-create the destination.');
-
-		ensureDir($destination, 'Could not auto-create the destination. Please create it and try again.');
-	}
 
 	// process the includes & excludes
-	$includes = array();
-	$included_paths = array();
+	$source = dirname(dirname(__FILE__));
+	$destination = mkPath($source, 'build');
+
+	$namespace = basename($source);
+	$includes = array($namespace);
+	$included_paths = array($namespace);
 
 	$excludes = array();
 	$excluded_paths = array();
@@ -518,23 +375,19 @@
 	$compiled_js = array();
 	$compiled_css = array();
 
-	$args = array_slice($argv, 3);
+	$args = array_slice($argv, 1);
 
 	foreach($args as $arg){
 		if($arg && $arg[0] == '-'){
 			$excludes[] = $arg = substr($arg, 1);
 
 			$excluded_paths[] = str_replace('.', DS, $arg);
-		} else{
+		}
+		else{
 			$includes[] = $arg;
 
 			$included_paths[] = str_replace('.', DS, $arg);
 		}
-	}
-
-	// make sure we have at least one namespace
-	if(!count($includes)){
-		endScript('You must have at least one included namespace.');
 	}
 
 	// confirm to continue
@@ -550,60 +403,19 @@
 
 
 
-	// we will hold off on processing the apps files until everything is compressed
-	// just do a straight copy of files for now
-	trace('Copying Apps...');
-
-	if(file_exists(mkPath($source, 'apps'))){
-		ensureCopy(
-			mkPath($source, 'apps'),
-			mkPath($destination, 'apps'),
-			'Could not copy the apps directory.'
-		);
-	}
-
-
-
-
-	// we don't need to do anything for assets folder except copy it
-	trace('Copying Assets...');
-
-	if(file_exists(mkPath($source, 'assets'))){
-		ensureDir(mkPath($destination, 'assets'), 'Could not create the assets directory in the destination.');
-
-		foreach($includes as $i){
-			ensureCopy(
-				mkPath($source, 'assets', $i),
-				mkPath($destination, 'assets', $i),
-				'Could not copy the assets directory.'
-			);
-		}
-	}
-
-
-
-
 	// copy all the css files so we don't mess up any originals
 	trace('Copying CSS...');
 
 	if(file_exists(mkPath($source, 'css'))){
 		ensureDir(mkPath($destination, 'css'), 'Could not create the css directory in the destination.');
 
-		foreach($includes as $i){
-			ensureCopy(
-				mkPath($source, 'css', $i),
-				mkPath($destination, 'css', $i),
-				'Could not copy the css directory.'
-			);
-		}
+		ensureCopy(mkPath($source, 'css'), mkPath($destination, 'css'), 'Could not copy the css directory.');
 	}
 
 	// process and compress every css file
 	trace('Compressing CSS...');
 
-	foreach($includes as $i){
-		compress(mkPath($destination, 'css', $i), 'Could not compress css at: ' . $i);
-	}
+	compress(mkPath($destination, 'css'), 'Could not compress the css.');
 
 
 
@@ -614,21 +426,13 @@
 	if(file_exists(mkPath($source, 'js'))){
 		ensureDir(mkPath($destination, 'js'), 'Could not create the js directory in the destination.');
 
-		foreach($includes as $i){
-			ensureCopy(
-				mkPath($source, 'js', $i),
-				mkPath($destination, 'js', $i),
-				'Could not copy the js directory.'
-			);
-		}
+		ensureCopy(mkPath($source, 'js'), mkPath($destination, 'js'), 'Could not copy the js directory.');
 	}
 
 	// process and compress each js file
 	trace('Compressing JS...');
 
-	foreach($includes as $i){
-		compress(mkPath($destination, 'js', $i), 'Could not compress js at: ' . $i);
-	}
+	compress(mkPath($destination, 'js'), 'Could not compress the js.');
 
 
 
@@ -639,21 +443,13 @@
 	if(file_exists(mkPath($source, 'templates'))){
 		ensureDir(mkPath($destination, 'templates'), 'Could not create the templates directory in the destination.');
 
-		foreach($includes as $i){
-			ensureCopy(
-				mkPath($source, 'templates', $i),
-				mkPath($destination, 'templates', $i),
-				'Could not copy the templates directory.'
-			);
-		}
+		ensureCopy(mkPath($source, 'templates'), mkPath($destination, 'templates'), 'Could not copy the templates directory.');
 	}
 
 	// process and compress each html file
 	trace('Compressing Templates...');
 
-	foreach($includes as $i){
-		compress(mkPath($destination, 'templates', $i), 'Could not compress template at: ' . $i);
-	}
+	compress(mkPath($destination, 'templates'), 'Could not compress the templates.');
 
 
 
@@ -662,101 +458,71 @@
 	trace('Copying Themes...');
 
 	if(file_exists(mkPath($source, 'themes'))){
-		ensureCopy(
-			mkPath($source, 'themes'),
-			mkPath($destination, 'themes'),
-			'Could not copy the themes directory.'
-		);
+		ensureCopy(mkPath($source, 'themes'), mkPath($destination, 'themes'), 'Could not copy the themes directory.');
 	}
 
 	// compress the theme css
 	trace('Compressing Themes...');
 
-	compress(mkPath($destination, 'themes'), 'Could not compress theme at: ' . $i);
+	compress($dir = mkPath($destination, 'themes'), 'Could not compress the themes.');
+
+	$dir = new DirectoryIterator($dir);
+
+	foreach($dir as $file){
+		if(!$file->isDot()){
+			$info = pathinfo($path = $file->getPathName());
+
+			copy($path, mkpath($source, str_replace('.' . $info['extension'], '', $info['basename']) . '-theme.css'));
+		}
+	}
+
 
 
 
 	// process and compress all the code
 	trace('Compiling...');
 
-	// compile each namespace
-	foreach($includes as $i){
-		// compile the js
-		$js = compileJs(mkPath($destination, 'js', $i), $i);
+	// compile the js
+	$js = compileJs(mkPath($destination, 'js'), $namespace);
 
-		// compile the css
-		file_put_contents(mkPath($destination, 'css', $i . '.css'), compileCss(mkPath($destination, 'css', $i), $js, $i));
+	// compile the css
+	file_put_contents(mkPath($source, $namespace . '.css'), compileCss(mkPath($destination, 'css'), $js, $namespace));
 
-		// now auto-create the namespace structure
-		$objs = new stdClass();
-		$root = mkPath($destination, 'js', $i, '');
-
-		foreach($compiled_js as $js_path){
-			if(strpos($js_path, $root) === FALSE){
-				continue;
-			}
-
-			$parts = explode(DS, str_replace(
-				array($root, '.js'),
-				array('', ''),
-				$js_path
-			));
-
-			$obj =& $objs;
-
-			foreach($parts as $part){
-				if(!isset($obj->$part)){
-					$obj->$part = new stdClass();
-				}
-
-				$obj =& $obj->$part;
-			}
-		}
-
-		// write the compiled js to its final location
-		// add one use strict for all the code
-		// add the namespace structure
-		// remove all the extra use stricts
-		// and perform code replacement optimizations
-		file_put_contents(
-			mkPath($destination, 'js', $i . '.js'),
-			'"use strict";' . str_replace(
-				array(
-				     '"_constructor"', '_constructor:', '._constructor',
-				     '"_destructor"', '_destructor:', '._destructor',
-				     '"_get_props_"', '_get_props_:', '._get_props_',
-				     '"_props_"', '_props_:', '._props_',
-				     '"_set_props_"', '_set_props_:', '._set_props_',
-				     '._static',
-				     '"_super"', '_super:', '._super(', '._super=',
-				     '"_template"', '_template:', '._template',
-				     '"_unset"', '_unset:', '._unset',
-				     '"use strict";',
-				),
-				array(
-				     '"_c"', '_c:', '._c',
-				     '"_d"', '_d:', '._d',
-				     '"_g_p_"', '_g_p_:', '._g_p_',
-				     '"_p_"', '_p_:', '._p_',
-				     '"_s_p_"', '_s_p_:', '._s_p_',
-				     '.$',
-				     '"_s"', '_s:', '._s(', '._s=',
-				     '"_t"', '_t:', '._t',
-				     '"_u"', '_u:', '._u',
-				     '',
-				),
-				$js
-			)
-		);
-	}
-
-
-
-
-	// update apps to use compressed and compiled files
-	trace('Updating Apps...');
-
-	updateApps(mkPath($destination, 'apps'));
+	// write the compiled js to its final location
+	// add one use strict for all the code
+	// add the namespace structure
+	// remove all the extra use stricts
+	// and perform code replacement optimizations
+	file_put_contents(
+		mkPath($source, $namespace . '.js'),
+		'"use strict";' . str_replace(
+			array(
+			     '"_constructor"', '_constructor:', '._constructor',
+			     '"_destructor"', '_destructor:', '._destructor',
+			     '"_get_props_"', '_get_props_:', '._get_props_',
+			     '"_props_"', '_props_:', '._props_',
+			     '"_set_props_"', '_set_props_:', '._set_props_',
+			     '._static',
+			     '"_super"', '_super:', '._super(', '._super=',
+			     '"_template"', '_template:', '._template',
+			     '"_unset"', '_unset:', '._unset',
+			     '"use strict";',
+			),
+			array(
+			     '"_c"', '_c:', '._c',
+			     '"_d"', '_d:', '._d',
+			     '"_g_p_"', '_g_p_:', '._g_p_',
+			     '"_p_"', '_p_:', '._p_',
+			     '"_s_p_"', '_s_p_:', '._s_p_',
+			     '.$',
+			     '"_s"', '_s:', '._s(', '._s=',
+			     '"_t"', '_t:', '._t',
+			     '"_u"', '_u:', '._u',
+			     '',
+			),
+			$js
+		)
+	);
 
 
 
